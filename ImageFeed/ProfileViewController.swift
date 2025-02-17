50205814c84cea0180a04eebdde565dd2973e979
Evgeniy Kostyaev <evgeniy.kostyaev@infobric.com>
2025-02-17 11:33:01 +0400

    Made up "Profile" page

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Evgeniy Kostyaev on 17.02.2025.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private weak var userPhotoImageView: UIImageView!
+    
+    @IBOutlet private weak var nameLabel: UILabel!
+    
+    @IBOutlet private weak var nicknameLabel: UILabel!
+    
+    @IBOutlet private weak var statusLabel: UILabel!
+    
+    @IBOutlet private weak var logoutButton: UIButton!
+}
