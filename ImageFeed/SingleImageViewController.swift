9b1bcf7e89cc6c82c3226e455cf39f072eca4d29
Evgeniy Kostyaev <evgeniy.kostyaev@infobric.com>
2025-02-17 14:53:13 +0400

    Added SingleImageViewController

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Evgeniy Kostyaev on 17.02.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+}
