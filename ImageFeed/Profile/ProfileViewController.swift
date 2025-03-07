765e663b5fc7a5a8fa1d9f796a0713984d673efa
Evgeniy <evgeniy.kostyaev@infobric.com>
2025-03-07 13:42:51 +0400

    Got rid of "Magic number" antipatern

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -7,6 +7,29 @@
 
 import UIKit
 
+struct ProfileViewControllerTheme {
+    static let nameLabelFont: CGFloat = 23
+    
+    static let nicknameLabelFont: CGFloat = 13
+    static let nicknameLabelTextColor: UIColor = UIColor(red: 174.0/255.0, green: 175.0/255.0, blue: 180.0/255.0, alpha: 1.0)
+    
+    static let statusLabelFont: CGFloat = 13
+    
+    static let logoutButtonCollor: UIColor = UIColor(red: 245.0/255.0, green: 107.0/255.0, blue: 108.0/255.0, alpha: 1.0)
+    
+    static let userPhotoImageViewHeight: CGFloat = 70
+    static let userPhotoImageViewWidth: CGFloat = 70
+    
+    static let userPhotoImageViewTopConstraint: CGFloat = 32
+    
+    static let commonLeadingConstraint: CGFloat = 16
+    static let commonTrailingConstraint: CGFloat = -16
+    static let commonTopConstraint: CGFloat = 8
+    
+    static let logoutButtonHeight: CGFloat = 44
+    static let logoutButtonWidth: CGFloat = 44
+}
+
 final class ProfileViewController: UIViewController {
     
     private let userPhotoImageView: UIImageView = {
@@ -15,7 +38,7 @@ final class ProfileViewController: UIViewController {
     
     private let nameLabel: UILabel = {
         let nameLabel = UILabel()
-        nameLabel.font = .systemFont(ofSize: 23, weight: .bold)
+        nameLabel.font = .systemFont(ofSize: ProfileViewControllerTheme.nameLabelFont, weight: .bold)
         nameLabel.textColor = .white
         nameLabel.text = "Екатерина Новикова"
         
@@ -24,8 +47,8 @@ final class ProfileViewController: UIViewController {
     
     private let nicknameLabel: UILabel = {
         let nicknameLabel = UILabel()
-        nicknameLabel.font = .systemFont(ofSize: 13, weight: .regular)
-        nicknameLabel.textColor = UIColor(red: 174.0/255.0, green: 175.0/255.0, blue: 180.0/255.0, alpha: 1.0)
+        nicknameLabel.font = .systemFont(ofSize: ProfileViewControllerTheme.nicknameLabelFont, weight: .regular)
+        nicknameLabel.textColor = ProfileViewControllerTheme.nicknameLabelTextColor
         nicknameLabel.text = "@ekaterina_nov"
         
         return nicknameLabel
@@ -33,7 +56,7 @@ final class ProfileViewController: UIViewController {
     
     private let statusLabel: UILabel = {
         let statusLabel = UILabel()
-        statusLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        statusLabel.font = .systemFont(ofSize: ProfileViewControllerTheme.statusLabelFont, weight: .regular)
         statusLabel.textColor = .white
         statusLabel.text = "Hello, world!"
         
@@ -46,7 +69,7 @@ final class ProfileViewController: UIViewController {
             target: self,
             action: #selector(didTapLogoutButton)
         )
-        logoutButton.tintColor = UIColor(red: 245.0/255.0, green: 107.0/255.0, blue: 108.0/255.0, alpha: 1.0)
+        logoutButton.tintColor = ProfileViewControllerTheme.logoutButtonCollor
         
         return logoutButton
     }()
@@ -73,26 +96,26 @@ final class ProfileViewController: UIViewController {
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            userPhotoImageView.heightAnchor.constraint(equalToConstant: 70),
-            userPhotoImageView.widthAnchor.constraint(equalToConstant: 70),
-            userPhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
-            userPhotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            userPhotoImageView.heightAnchor.constraint(equalToConstant: ProfileViewControllerTheme.userPhotoImageViewHeight),
+            userPhotoImageView.widthAnchor.constraint(equalToConstant: ProfileViewControllerTheme.userPhotoImageViewWidth),
+            userPhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: ProfileViewControllerTheme.userPhotoImageViewTopConstraint),
+            userPhotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ProfileViewControllerTheme.commonLeadingConstraint),
             
-            nameLabel.topAnchor.constraint(equalTo: userPhotoImageView.bottomAnchor, constant: 8),
-            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            nameLabel.topAnchor.constraint(equalTo: userPhotoImageView.bottomAnchor, constant: ProfileViewControllerTheme.commonTopConstraint),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ProfileViewControllerTheme.commonLeadingConstraint),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: ProfileViewControllerTheme.commonTrailingConstraint),
             
-            nicknameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
-            nicknameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            nicknameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            nicknameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: ProfileViewControllerTheme.commonTopConstraint),
+            nicknameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ProfileViewControllerTheme.commonLeadingConstraint),
+            nicknameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: ProfileViewControllerTheme.commonTrailingConstraint),
             
-            statusLabel.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: 8),
-            statusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            statusLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            statusLabel.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: ProfileViewControllerTheme.commonTopConstraint),
+            statusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ProfileViewControllerTheme.commonLeadingConstraint),
+            statusLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: ProfileViewControllerTheme.commonTrailingConstraint),
             
-            logoutButton.heightAnchor.constraint(equalToConstant: 44),
-            logoutButton.widthAnchor.constraint(equalToConstant: 44),
-            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            logoutButton.heightAnchor.constraint(equalToConstant: ProfileViewControllerTheme.logoutButtonHeight),
+            logoutButton.widthAnchor.constraint(equalToConstant: ProfileViewControllerTheme.logoutButtonWidth),
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: ProfileViewControllerTheme.commonTrailingConstraint),
             logoutButton.centerYAnchor.constraint(equalTo: userPhotoImageView.centerYAnchor)
         ])
     }
