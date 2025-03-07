b62c6d27a00f001d4bed67608a967055a0396ef3
Evgeniy <evgeniy.kostyaev@infobric.com>
2025-03-07 13:05:30 +0400

    Fixed bug after code review[part#3]

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,117 +9,92 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    private var userPhotoImageView: UIImageView?
+    private let userPhotoImageView: UIImageView = {
+        return UIImageView(image: UIImage(named: "user_photo_icon"))
+    }()
     
-    private var nameLabel: UILabel?
-    
-    private var nicknameLabel: UILabel?
-    
-    private var statusLabel: UILabel?
-    
-    private var logoutButton: UIButton?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupUserPhotoImageView()
-        
-        setupNameLabel()
-        
-        setupNicknameLabel()
-        
-        setupStatusLabel()
-        
-        setupLogoutButton()
-    }
-    
-    // MARK: - Helper methods
-    private func setupUserPhotoImageView() {
-        let userPhotoImageView = UIImageView(image: UIImage(named: "user_photo_icon"))
-        userPhotoImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(userPhotoImageView)
-        
-        userPhotoImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
-        userPhotoImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
-        userPhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
-        userPhotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        
-        self.userPhotoImageView = userPhotoImageView
-    }
-    
-    private func setupNameLabel() {
+    private let nameLabel: UILabel = {
         let nameLabel = UILabel()
         nameLabel.font = .systemFont(ofSize: 23, weight: .bold)
         nameLabel.textColor = .white
         nameLabel.text = "Екатерина Новикова"
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(nameLabel)
         
-        if let userPhotoImageView = userPhotoImageView {
-            nameLabel.topAnchor.constraint(equalTo: userPhotoImageView.bottomAnchor, constant: 8).isActive = true
-        }
-        
-        nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 16).isActive = true
-        
-        self.nameLabel = nameLabel
-    }
+        return nameLabel
+    }()
     
-    private func setupNicknameLabel() {
+    private let nicknameLabel: UILabel = {
         let nicknameLabel = UILabel()
         nicknameLabel.font = .systemFont(ofSize: 13, weight: .regular)
         nicknameLabel.textColor = UIColor(red: 174.0/255.0, green: 175.0/255.0, blue: 180.0/255.0, alpha: 1.0)
         nicknameLabel.text = "@ekaterina_nov"
-        nicknameLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(nicknameLabel)
         
-        if let nameLabel = nameLabel {
-            nicknameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
-        }
-        
-        nicknameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        nicknameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 16).isActive = true
-        
-        self.nicknameLabel = nicknameLabel
-    }
+        return nicknameLabel
+    }()
     
-    private func setupStatusLabel() {
+    private let statusLabel: UILabel = {
         let statusLabel = UILabel()
         statusLabel.font = .systemFont(ofSize: 13, weight: .regular)
         statusLabel.textColor = .white
         statusLabel.text = "Hello, world!"
-        statusLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(statusLabel)
         
-        if let nicknameLabel = nicknameLabel {
-            statusLabel.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: 8).isActive = true
-        }
-        
-        statusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        statusLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 16).isActive = true
-        
-        self.statusLabel = statusLabel
-    }
+        return statusLabel
+    }()
     
-    private func setupLogoutButton() {
+    private lazy var logoutButton: UIButton = {
         let logoutButton = UIButton.systemButton(
             with: UIImage(named: "logout_icon") ?? UIImage(),
             target: self,
             action: #selector(didTapLogoutButton)
         )
-        
         logoutButton.tintColor = UIColor(red: 245.0/255.0, green: 107.0/255.0, blue: 108.0/255.0, alpha: 1.0)
-        logoutButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(logoutButton)
         
-        logoutButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
-        logoutButton.widthAnchor.constraint(equalToConstant: 44).isActive = true
-        logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        return logoutButton
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        if let userPhotoImageView = userPhotoImageView {
-            logoutButton.centerYAnchor.constraint(equalTo: userPhotoImageView.centerYAnchor).isActive = true
+        setupViews()
+        
+        setupConstraints()
+    }
+    
+    // MARK: - Helper methods
+    private func setupViews() {
+        [userPhotoImageView,
+         nameLabel,
+         nicknameLabel,
+         statusLabel,
+         logoutButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
         }
-        
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            userPhotoImageView.heightAnchor.constraint(equalToConstant: 70),
+            userPhotoImageView.widthAnchor.constraint(equalToConstant: 70),
+            userPhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            userPhotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            nameLabel.topAnchor.constraint(equalTo: userPhotoImageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            nicknameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            nicknameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            nicknameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            statusLabel.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: 8),
+            statusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            statusLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            logoutButton.heightAnchor.constraint(equalToConstant: 44),
+            logoutButton.widthAnchor.constraint(equalToConstant: 44),
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            logoutButton.centerYAnchor.constraint(equalTo: userPhotoImageView.centerYAnchor)
+        ])
     }
     
     // MARK: - Action methods
