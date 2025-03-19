d55664ad201b673e8b94c72d6dcdb6c35b27bf9f
Evgeniy <evgeniy.kostyaev@infobric.com>
2025-03-19 13:19:36 +0400

    [sprint_10] - Implemented "loadAuthView" in "WebViewViewController"

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
-    private let showWebViewSegueIdentifier = "ShowWebView"
-    
     // MARK: - IB Outlets
     @IBOutlet weak private var loginButton: UIButton!
+    
+    // MARK: - Private Properties
+    private let showWebViewSegueIdentifier = "ShowWebView"
+    
+    // MARK: - Overrides Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureBackButton()
+    }
+    
+    // MARK: - Private Methods
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: String(), style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "ypBlack")
+    }
 }
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -6,7 +6,43 @@
 //
 
 import UIKit
+import WebKit
+
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+} 
 
 final class WebViewViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak private var webView: WKWebView!
+    
+    // MARK: - Overrides Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadAuthView()
+    }
+    
+    // MARK: - Private Methods
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        guard let url = urlComponents.url else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
     
 }
