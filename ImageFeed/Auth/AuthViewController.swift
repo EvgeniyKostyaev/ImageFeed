e769534cac22ac4fa8cde882ff1957204ad11fac
Evgeniy <evgeniy.kostyaev@infobric.com>
2025-03-19 11:52:10 +0400

    [sprint_10] - Created "AuthViewController" view controller

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Evgeniy Kostyaev on 19.03.2025.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak private var loginButton: UIButton!
+    
+    // MARK: - IB Actions
+    @IBAction private func didTapLoginButton(_ sender: Any) {
+        
+    }
+}
