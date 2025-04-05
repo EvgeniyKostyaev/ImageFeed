42af1cd19d709038a43f3f105b0cc54201e9f73b
Evgeniy <evgeniy.kostyaev@infobric.com>
2025-04-05 14:09:31 +0400

    [sprint_11] - Created "ProfileResult" and "ProfileService"

---
+++ ImageFeed/Models/ProfileResult.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileResult.swift
+//  ImageFeed
+//
+//  Created by Evgeniy Kostyaev on 03.04.2025.
+//
+
+import Foundation
+
+struct ProfileResult: Decodable {
+    
+}
---
+++ ImageFeed/Service/ProfileService.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileService.swift
+//  ImageFeed
+//
+//  Created by Evgeniy Kostyaev on 03.04.2025.
+//
+
+import Foundation
+
+final class ProfileService {
+    
+}
+
