ebf2bc63d018ab0a24af7228d7e72560fd11f15f
Evgeniy <evgeniy.kostyaev@infobric.com>
2025-04-09 16:22:30 +0400

    [sprint_11] - Added changes for "TabBarController"

--- ImageFeed/TabBarController.swift
+++ ImageFeed/TabBarController.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import UIKit
- 
+
 final class TabBarController: UITabBarController {
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,8 +17,11 @@ final class TabBarController: UITabBarController {
             withIdentifier: "ImagesListViewController"
         )
         
-        let profileViewController = storyboard.instantiateViewController(
-            withIdentifier: "ProfileViewController"
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil
         )
         
         self.viewControllers = [imagesListViewController, profileViewController]
